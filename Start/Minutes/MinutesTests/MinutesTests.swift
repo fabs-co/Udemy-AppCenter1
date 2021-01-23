cdd67796245e00d2b3775efe430b9baa5e44666d
Fabian <fabianplopez@outloook.com>
2021-01-22 20:49:45 -0500

    added unit tests

--- Start/Minutes/MinutesTests/MinutesTests.swift
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -7,27 +7,26 @@
 //
 
 import XCTest
+@testable import Minutes
 
 class MinutesTests: XCTestCase {
+    var entryUnderTest : Entry!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp(){
+        super.setUp()
+        entryUnderTest = Entry("Title", "Content")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        entryUnderTest = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSerialization(){
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
